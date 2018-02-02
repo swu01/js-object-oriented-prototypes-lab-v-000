00e19482030ae78970c0e51919e2caa4bbc41cfe
Sammy Wu <swucode@gmail.com>
2018-02-02 06:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ BoardMember.prototype.approve = function(){
     return 'You can do that!';
 }
 BoardMember.prototype.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
+  return "I like to help people.";
+}
+BoardMember.prototype.releasePressStatement = function(){
 //     return "You will see great things from Scuber."
 //   }
 //
