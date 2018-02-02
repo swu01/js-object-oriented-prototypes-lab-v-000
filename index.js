4f0dccfae63114a1f5e9ea23491dd5afd4f85370
Sammy Wu <swucode@gmail.com>
2018-02-02 06:26:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
-//   this.doCharity = function(){
+function BoardMember(name, homeState, training){
+   this.name = name;
+   this.homeState = homeState;
+   this.training = training;
+ }
+
+BoardMember.prototype.veto = function(){
+    return 'No, I must disagree';
+  }
+BoardMember.prototype.approve = function(){
+    return 'You can do that!';
+}
+BoardMember.prototype.doCharity = function(){
 //     return "I like to help people."
 //   }
 //
